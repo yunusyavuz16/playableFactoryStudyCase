a03ce270259cc5996ddcac7541ed3b684800a723
yunusyavuz16 <76525954+yunusyavuz16@users.noreply.github.com>
2023-10-17 20:59:52 +0300

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  secret: "7w!z%C*F-JaNdRgUjXn2r5u8x/A?D(G+",
+  secret: "",
   connectionString:
-    "mongodb+srv://playableFactory34:kfxkm0XJcmT0zjo-U34@cluster0.kdywu1c.mongodb.net/?retryWrites=true&w=majority",
+   "",
   dbName: "db_playableFactory",
 };
